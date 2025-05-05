f1e926de5d92367de20e4a570c6ba9d3660beb0c
Rajan66 <rajanmaharjan042@gmail.com>
2025-05-05 18:41:39 +0545

    feat(bot): basic message setup:

---
+++ README.md
@@ -0,0 +1,7 @@
+# ChunChunMaru - A BotCat
+
+#### runs the script as a module instead of just top-level script
+
+#### relative imports is possible if we run the script/project using -m
+
+`python -m bot.bot`
--- bot/bot.py
+++ bot/bot.py
@@ -18,10 +18,7 @@ def greet(bot):
     print(f"{bot} to the rescue")
 
 
-@client.event
-async def on_ready():
-    greet(client.user)
-
+def find_guild(client):
     for guild in client.guilds:
         if guild.name == GUILD:
             break
@@ -31,10 +28,47 @@ async def on_ready():
         print(f"{guild.name} has the ID: {guild.id}")
     else:
         print(f"{GUILD} not found.")
+    return guild
+
+
+@client.event
+async def on_ready():
+    greet(client.user)
+    # guild = find_guild(client)
+
+    # using find()
+    # - takes predicate (another func) i.e. lambda func in this case
+    # - and an iterable (list)
+    # guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    guild = discord.utils.get(client.guilds, name=GUILD)
 
     for member in guild.members:
         if member.name in dog_members:
             print(f"{member.name} is a doggy bhai")
+
+
+@client.event
+async def on_message(message):
+    print(f"{message.author} is sending a message")
+
+    if message.author == client.user:
+        return
+
+    if message.content == "bleh":
+        response = "bleh bleh bleh"
+        await message.channel.send(response)
+
+    if str(message.author) == "icecoldreaper" and message.content == "yo chunchunmaru":
+        response = "hi boss"
+        await message.channel.send(response)
+
+    if str(message.author) == "x_lord":
+        response = "dhwang khatey chup lol"
+        await message.channel.send(response)
+
+    if message.content.startswith("h"):
+        response = "chunchunmaru"
+        await message.channel.send(response)
 
 
 client.run(TOKEN)
